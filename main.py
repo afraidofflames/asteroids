5ebb4ebbc5f31ed119d40d0aec087057d061ddf2
afraidofflames <afraidofflames@live.com.au>
2025-02-23 22:10:01 +1000

    added player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 
 def main():
     pygame.init()
@@ -10,12 +11,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while true:
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt=0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0,0,0)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt=clock.tick(60)/1000
 
 
 if __name__ == "__main__":
